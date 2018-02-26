e3f43fce075ddf69806d3f6eeca5acf05f9388c5
sunny <2385585770@qq.com>
2018-02-26 18:14:25 +0800

    添加：模板方法模式代码

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ java中的设计模式
 [×] 单例模式
 
 [√] [观察者模式](./src/main/java/com/sunny/observer/readme.md)
+
+[√] [模板方法模式](./src/main/java/com/sunny/templateMethod/readme.md)
---
+++ src/main/java/com/sunny/templateMethod/AbstractPerson.java
@@ -0,0 +1,34 @@
+package com.sunny.templateMethod;
+
+/**
+ * @author sunny
+ * @className AbstractPerson
+ * @date 2018-02-26 18:03
+ * @description: 举个例子，以准备去学校所要做的工作（prepareGotoSchool）为例，假设需要分三步：穿衣服（dressUp），吃早饭（eatBreakfast），带上东西（takeThings）。学生和老师要做得具体事情肯定有所区别。
+ */
+public abstract class AbstractPerson {
+    /**
+     * 抽象类定义整个流程骨架
+     */
+    public final void preparGoToSchool() {
+        dressUp();
+        eatBreakfast();
+        takeThings();
+    }
+    //以下是不同子类根据自身特性完成的具体步骤
+
+    /**
+     * 穿衣服
+     */
+    protected abstract void dressUp();
+
+    /**
+     * 吃早饭
+     */
+    protected abstract void eatBreakfast();
+
+    /**
+     * 带上东西
+     */
+    protected abstract void takeThings();
+}

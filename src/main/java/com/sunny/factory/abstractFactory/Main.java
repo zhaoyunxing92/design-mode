0869b1b9545343c8adb6a9eafe18b728c56be401
sunny <2385585770@qq.com>
2018-02-27 21:29:36 +0800

    添加：静态工厂方法

---
+++ src/main/java/com/sunny/factory/abstractFactory/Main.java
@@ -0,0 +1,13 @@
+package com.sunny.factory.abstractFactory;
+
+/**
+ * @author sunny
+ * @className Main
+ * @date 2018-02-27 18:11
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
